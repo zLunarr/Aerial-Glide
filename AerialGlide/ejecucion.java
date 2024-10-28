4d24a0a7c17d416be720e8b84ef9ed0e3f628304
Ivancai7L <132943824+Ivancai7L@users.noreply.github.com>
2024-10-28 20:16:30 -0300

    Update ejecucion.java

--- AerialGlide/ejecucion.java
+++ AerialGlide/ejecucion.java
@@ -1,6 +1,6 @@
 package juegojava;
 
-import java.awt.Component;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
@@ -9,18 +9,21 @@ public class ejecucion {
         // Crear la ventana del juego
         JFrame frame = new JFrame();
         // Crear el panel del juego
-        juegoPanel juego = new juegoPanel(); // Asegúrate de que esta clase extienda JPanel
+        juegoPanel juego = new juegoPanel(); 
 
         // Agregar el panel del juego a la ventana
         frame.add(juego);
-        // Configurar el título de la ventana
+        juego.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()));
+        frame.pack(); // Ajusta el tamaño de la ventana a la del panel
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // Título de la ventana
         frame.setTitle("Aerial Glide");
-        // Configurar para cerrar la aplicación al salir
+        // Cerrar la aplicación al salir
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Evitar que la ventana sea redimensionable
         frame.setResizable(false);
-        // Establecer el tamaño de la ventana
-        frame.setSize(400, 600);
+        // Tamaño de la ventana
+        frame.setSize(2000, 1800);
         // Centrar la ventana en la pantalla
         frame.setLocationRelativeTo(null);
         // Mostrar la ventana
