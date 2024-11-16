7c83d05232e221cf12e9ab141ef0b61d870f7fa6
Ivancai7L <132943824+Ivancai7L@users.noreply.github.com>
2024-11-15 23:54:44 -0300

    Update obstaculos.java

--- AerialGlide/obstaculos.java
+++ AerialGlide/obstaculos.java
@@ -1,5 +1,33 @@
 package juegojava;
 
-public class obstaculos {
+import java.awt.*;
+import javax.swing.*;
 
+public class Obstaculos {
+    private int x, y, ancho, alto;
+    private final Image imagen;
+
+    public Obstaculos(int x, int y, int ancho, int alto, String rutaImagen) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+        this.imagen = new ImageIcon(rutaImagen).getImage();
+    }
+
+    public void mover(int velocidad) {
+        x -= velocidad;
+    }
+
+    public boolean fueraDePantalla() {
+        return x + ancho < 0;
+    }
+
+    public void dibujar(Graphics g) {
+        g.drawImage(imagen, x, y, ancho, alto, null);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, ancho, alto);
+    }
 }
